87f904ea284fbd03cb81a0bba39a9f855d0ad7d3
Vitor Avelar Martins <vitor.martins@sou.unifeob.edu.br>
2019-09-23 22:30:24 -0300

    Primeiro Committ

--- src/Faculdade.java
+++ src/Faculdade.java
@@ -2,14 +2,24 @@ import java.util.ArrayList;
 
 public class Faculdade {
 	ArrayList<Aluno> dados = new ArrayList<>();
+	double valor = 0;
 
 	public void medias() {
-		//int cont= 0;
+		int cont= 0;
 		for (Aluno alunos1 : dados) {
 			System.out.println(alunos1.nome + " " + alunos1.cursoAluno.nomeCurso + " " + alunos1.serieAluno.serie + " "
-					+ alunos1.notaAluno.NotaDiciplina+" "+alunos1.notaAluno.nota);
-		
-			
+					+ alunos1.notaAluno.NotaDiciplina + " " + alunos1.notaAluno.nota);
+			cont=cont+1;
+
+			if (alunos1.cursoAluno.nomeCurso == "ADS") {
+				
+				valor=valor+alunos1.notaAluno.nota;
+
+				System.out.println(valor/cont);
+				
+
+			}
+
 		}
 	}
 
